54572ca5c78238c674c4242dc5e84463736355a5
Valentin Jubert <valentin.jubert@ideine.fr>
2019-04-30 13:56:36 +0200

    add ios resources & fix some layout issues

--- AlbumLieux/AlbumLieux/ViewModels/MainViewModel.cs
+++ AlbumLieux/AlbumLieux/ViewModels/MainViewModel.cs
@@ -10,92 +10,99 @@ using Xamarin.Forms;
 
 namespace AlbumLieux.ViewModels
 {
-    public class MainViewModel : ViewModelBase
-    {
-        private readonly Lazy<IPlacesDataServices> _placesService = new Lazy<IPlacesDataServices>(() => DependencyService.Get<IPlacesDataServices>());
-        private readonly Lazy<ITokenService> _tokenService = new Lazy<ITokenService>(() => DependencyService.Get<ITokenService>());
+	public class MainViewModel : ViewModelBase
+	{
+		private readonly Lazy<IPlacesDataServices> _placesService = new Lazy<IPlacesDataServices>(() => DependencyService.Get<IPlacesDataServices>());
+		private readonly Lazy<ITokenService> _tokenService = new Lazy<ITokenService>(() => DependencyService.Get<ITokenService>());
 
-        public ICommand ItemSelectedCommand { get; }
-        public ICommand RefreshCommand { get; }
-        public ICommand ProfileCommand { get; }
+		public ICommand RefreshCommand { get; }
+		public ICommand ProfileCommand { get; }
 
-        private bool _isBusy;
+		private bool _isBusy;
 
-        public bool IsBusy
-        {
-            get => _isBusy;
-            set => SetProperty(ref _isBusy, value);
-        }
+		public bool IsBusy
+		{
+			get => _isBusy;
+			set => SetProperty(ref _isBusy, value);
+		}
 
-        private Places _selectedPlace;
+		private Places _selectedPlace;
 
-        public Places SelectedPlace
-        {
-            get => _selectedPlace;
-            set
-            {
-                if (SetProperty(ref _selectedPlace, value) && value != null)
-                {
-                    OnItemClicked(value);
-                }
-            }
-        }
+		public Places SelectedPlace
+		{
+			get => _selectedPlace;
+			set
+			{
+				if (SetProperty(ref _selectedPlace, value) && value != null)
+				{
+					OnItemClicked(value);
+				}
+			}
+		}
 
+		private List<Places> _spotList;
 
+		public List<Places> SpotList
+		{
+			get => _spotList;
+			set => SetProperty(ref _spotList, value);
+		}
 
-        private List<Places> _spotList;
+		public MainViewModel()
+		{
+			RefreshCommand = new Command(RefreshAction);
+			ProfileCommand = new Command(ProfileAction);
+		}
 
-        public List<Places> SpotList
-        {
-            get => _spotList;
-            set => SetProperty(ref _spotList, value);
-        }
+		public override async Task OnResume()
+		{
+			try
+			{
+				await base.OnResume();
 
-        public MainViewModel()
-        {
-            RefreshCommand = new Command(RefreshAction);
-            ProfileCommand = new Command(ProfileAction);
-        }
+				if (SpotList == null)
+				{
+					IsBusy = true;
+					SpotList = await _placesService.Value.ListPlaces();
+				}
 
-        public override async Task OnResume()
-        {
-            await base.OnResume();
+				SelectedPlace = null;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+		}
 
-            if (SpotList == null)
-            {
-                IsBusy = true;
-                SpotList = await _placesService.Value.ListPlaces();
-                IsBusy = false;
-            }
-            SelectedPlace = null;
+		private async void ProfileAction()
+		{
+			if (_tokenService.Value.HasToken())
+			{
+				await NavigationService.PushAsync<ProfilePage>();
+			}
+			else
+			{
+				await NavigationService.PushAsync<LoginPage>();
+			}
+		}
 
-        }
+		private async void RefreshAction()
+		{
+			IsBusy = true;
+			SpotList = await _placesService.Value.ListPlaces(true);
+			IsBusy = false;
+		}
 
-        private async void ProfileAction()
-        {
-            if (_tokenService.Value.HasToken())
-            {
-                await NavigationService.PushAsync<ProfilePage>();
-            }
-            else
-            {
-                await NavigationService.PushAsync<LoginPage>();
-            }
-        }
-
-        private async void RefreshAction()
-        {
-            IsBusy = true;
-            SpotList = await _placesService.Value.ListPlaces(true);
-            IsBusy = false;
-        }
-
-        private async void OnItemClicked(Places obj)
-        {
-            await NavigationService.PushAsync<DetailPage>(new Dictionary<string, object>
-            {
-                ["id"] = obj.Id
-            });
-        }
-    }
+		private async void OnItemClicked(Places obj)
+		{
+			await NavigationService.PushAsync<DetailPage>(new Dictionary<string, object>
+			{
+				["id"] = obj.Id
+			});
+		}
+	}
 }
